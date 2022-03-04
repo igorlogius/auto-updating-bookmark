f37b9cf2a8cc98d36ea2e3f99de69074e22fa919
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-03-04 15:32:04 +0100

    feat: add option to set the default save location

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
 
+//const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
+const manifest = browser.runtime.getManifest();
+const extname = manifest.name;
+
 const postfix = '#trackmark';
 let marks = {};
 
@@ -8,20 +12,20 @@ const debug = (msg) => {
 }
 */
 
-const onRemove = (tabId, removeInfo) => {
+function onRemove (tabId, removeInfo) {
 	if (marks[tabId]) {
-		//debug(`stopped tracking for tabId: ${tabId}`);
+		//console.debug(`stopped tracking for tabId: ${tabId}`);
 		delete marks[tabId];
 	}
 }
 
-const onUpdate = async (tabId, changeInfo, tabInfo) => {
+async function onUpdate (tabId, changeInfo, tabInfo) {
 
 	const tabUrl = tabInfo.url;
 	const tabTitle = tabInfo.title;
 
 	if(tabUrl.endsWith(postfix)){
-		//debug(`started tracking for tabId: ${tabId}`);
+		//console.debug(`started tracking for tabId: ${tabId}`);
 		marks[tabId] = tabUrl.slice(0, -postfix.length);
 		return;
 	}
@@ -31,13 +35,13 @@ const onUpdate = async (tabId, changeInfo, tabInfo) => {
 		const n_url = new URL(tabUrl);
 		const p_url = new URL(marks[tabId]);
 
-		// check if we are on the same origin 
+		// check if we are on the same origin
 		if( n_url.origin !== p_url.origin ) {
 			onRemove(tabId);
 			return;
 		}
 
-		//debug(`tab: ${tabId} - url changed from ${marks[tabId]} to ${tabUrl}`);
+		//console.debug(`tab: ${tabId} - url changed from ${marks[tabId]} to ${tabUrl}`);
 
 		const bmark = await browser.bookmarks.search({ url: marks[tabId] + postfix });
 		if(bmark.length > 0) {
@@ -47,13 +51,63 @@ const onUpdate = async (tabId, changeInfo, tabInfo) => {
 	}
 }
 
-const onClicked = (tab) => {
-  browser.bookmarks.create({ title: "auto:" + tab.title, url: tab.url + postfix });
-  browser.tabs.update(tab.id, { url: tab.url + postfix });
+async function onClicked (tab) {
+    let tmp;
+    try {
+        tmp = await browser.storage.local.get(extname);
+        tmp = tmp[extname];
+    }catch(e){
+        tmp = undefined;
+    }
+    browser.bookmarks.create({ parentId: tmp, title: "auto:" + tab.title, url: tab.url + postfix });
+    browser.tabs.update(tab.id, { url: tab.url + postfix });
 }
+
+
+browser.menus.create({
+	id: extname,
+	title: 'Check to save DAU bookmarks here',
+	type: "radio",
+	contexts: ["bookmark"],
+	visible: false,
+	checked: false,
+	onclick: async function(info, tab) {
+		if(info.bookmarkId ) {
+			//blub = { 'startup-tabs': info.bookmarkId }
+
+			let tmp = await browser.storage.local.get(extname);
+			if (tmp) {
+				tmp = tmp[extname];
+			}
+			let blub = {};
+			blub[extname] = (info.bookmarkId === tmp)? undefined: info.bookmarkId;
+			browser.storage.local.set(blub);
+		}
+	}
+});
+
+browser.menus.onShown.addListener(async function(info, tab) {
+	if(info.bookmarkId ) {
+		const bmn = (await browser.bookmarks.get(info.bookmarkId))[0];
+		if(!bmn.url) {
+			let tmp = await browser.storage.local.get(extname);
+			if (tmp) { tmp = tmp[extname]; }
+			browser.menus.update(extname, {visible: true, checked: (tmp === info.bookmarkId)});
+		}else{
+			browser.menus.update(extname, {visible: false, checked: false});
+		}
+	}
+	browser.menus.refresh();
+});
+
+const filter = {
+    urls: ["<all_urls>"],
+    properties: ["url"]
+}
+
 
 // add listeners
 browser.browserAction.onClicked.addListener(onClicked);
-browser.tabs.onUpdated.addListener(onUpdate); 
-browser.tabs.onRemoved.addListener(onRemove); 
+browser.tabs.onUpdated.addListener(onUpdate, filter);
+browser.tabs.onRemoved.addListener(onRemove);
 
